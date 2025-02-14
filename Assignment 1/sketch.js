b06fa5e29381a0e04d4daaae874064ca1667ee9a
BishalGhose <bishalghose00@gmail.com>
2025-02-14 12:16:59 -0600

    starting drawing house and finished terrain generation

--- Assignment 1/sketch.js
+++ Assignment 1/sketch.js
@@ -25,28 +25,38 @@ function draw() {
 
   drawStars(1.2, seed);
   drawTerrain(seed);
+  drawHouse();
   console.log(random(1,10));
 }
 
 function drawStars(rate, seedNumber) {
   noStroke();
   randomSeed(seedNumber);
-  for (let stars = 0; stars <= random(20,30) * rate; stars++){
-    let randomSizeOfCircle = random(40,60)/10;
-    circle(random(0,windowHeight), random(0,windowWidth/2), randomSizeOfCircle);
+  for (let stars = 0; stars <= random(28,40) * rate; stars++){
     fill(255,255,0,random(100,255));
+    let randomSizeOfCircle = random(40,60)/10;
+    circle(random(0,windowHeight), random(0,windowWidth/1.3), randomSizeOfCircle);
   }
 }
 
 
 function drawTerrain(seedNumber) {
   fill(255,255,0);
-  circle(windowHeight/2,windowWidth, windowHeight, windowWidth-windowWidth/10);
-  for (let blocks = 0; blocks <= 70; blocks++){
+  for (let chunks = 0; chunks <= 70; chunks++){
     randomDarkness = random(0,100);
     fill(randomDarkness,random(200,255),randomDarkness);
-    circle(random(0,windowHeight), random(windowWidth/1.5, windowWidth),random(160,350));
+    circle(random(0,windowHeight), random(windowWidth-chunks, windowWidth),random(200,400));
   }
+}
+
+function drawHouse(){
+  let randomXPos = random(75,windowWidth-75);
+  fill(255, 229, 180);
+  square(randomXPos,windowWidth/1.4,150);
+  fill(165, 42, 42);
+  rect(randomXPos + 50, (windowWidth/1.4) + 80,50,70);
+  fill(0,0,0);
+  circle(randomXPos + 90, (windowWidth/1.4) + 121, 5);
 }
 
 function mouseClicked(){
