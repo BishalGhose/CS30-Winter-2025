baff824f65c3f39c6f9f560764ed707d1938542b
BishalGhose <bishalghose00@gmail.com>
2025-02-14 11:08:43 -0600

    worked on scene assignment and lesson 4

--- Assignment 1/sketch.js
+++ Assignment 1/sketch.js
@@ -7,7 +7,6 @@
 
 
 
-
 let globalStatus;
 let seed = 1;
 
@@ -24,26 +23,30 @@ function draw() {
   
   background(0);
 
-
   drawStars(1.2, seed);
-  console.log(random(1,10))
+  drawTerrain(seed);
+  console.log(random(1,10));
 }
 
 function drawStars(rate, seedNumber) {
-  fill(255,255,0);
-  strokeWeight(1);
-  stroke(255,255,0);
+  noStroke();
   randomSeed(seedNumber);
   for (let stars = 0; stars <= random(20,30) * rate; stars++){
-    fill(255,255,0);
-    strokeWeight(1);
-    let randomSizeOfCircle = random(40,60)/10
+    let randomSizeOfCircle = random(40,60)/10;
     circle(random(0,windowHeight), random(0,windowWidth/2), randomSizeOfCircle);
-    fill(255,255,255);
-    circle(random(0,windowHeight), random(0,windowWidth/2), randomSizeOfCircle*2.9);
-
+    fill(255,255,0,random(100,255));
   }
-  randomSeed();
+}
+
+
+function drawTerrain(seedNumber) {
+  fill(255,255,0);
+  circle(windowHeight/2,windowWidth, windowHeight, windowWidth-windowWidth/10);
+  for (let blocks = 0; blocks <= 70; blocks++){
+    randomDarkness = random(0,100);
+    fill(randomDarkness,random(200,255),randomDarkness);
+    circle(random(0,windowHeight), random(windowWidth/1.5, windowWidth),random(160,350));
+  }
 }
 
 function mouseClicked(){
