54fa230be175a5cd5a85ea48de3063738c12d08a
BishalGhose <bishalghose00@gmail.com>
2025-03-18 12:16:59 -0600

    Worked on 15

---
+++ 15 Object-Object interactions connected nodes/sketch.js
@@ -0,0 +1,110 @@
+// Connected Nodes
+// Bishal Ghose
+// March 18
+// OOP Review + Object-Object Interactions
+
+//Global Variables
+let nodes = []; //to hold all of our objects
+let reach = 150; //how far connections b/w points may be
+
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  if (mouseIsPressed){
+    nodes.push(new csNode(mouseX, mouseY));
+  }
+  background(220);
+  for (let n of nodes) {
+    //loping by item is good here, as we don't
+    //plan on deleting any objects from the array
+
+    n.move();
+    n.connect(nodes);
+    n.display();
+  }
+}
+
+function mousePressed(){
+  //create one node per mouse press
+  //nodes.push(new csNode(mouseX, mouseY));
+}
+
+
+class csNode{
+  //1.constructor
+  constructor(x, y){ //called once per object created
+    //properties related to position/rendering
+    this.x = x; this.y = y; this.size = 5;
+    this.c = color(random(255), random(255), random(255));
+
+
+
+    //properties related to motion
+    this.xTime = random(10); this.yTime = random(10);
+    this.timeShift = 0.01; this. maxSpeed = 5;
+
+
+
+  }
+
+
+  //2. class methods
+  display(){
+    fill(this.c);
+    noStroke();
+
+    circle(this.x, this.y, this.size);
+  }
+
+  connect(nodeArray){
+    //check if the current point is close to any
+    //other points, and if so join with a line
+
+    stroke(this.c);
+    for(let n of nodes){
+      //this.x this,y   n.x n.y
+      if(n !== this){
+        let d = dist(this.x, this.y, n.x, n.y);
+        if(d < reach){ //the two points are close
+          line(this.x, this.y,n.x, n.y);         
+        }
+      }
+    }
+  }
+
+
+
+  move(){
+    //use perlin noise for x/y movement
+    let xSpeed = noise(this.xTime);  //0-1
+    xSpeed = map(xSpeed, 0, 1, -this.maxSpeed, this.maxSpeed);
+    this.xTime += this.timeShift;
+    this.x += xSpeed;
+    if(this.x < 0){
+      this.x = width;
+    }
+    else if (this.x > width){
+      this.x = 0;
+    }
+
+    let ySpeed = noise(this.yTime);
+    ySpeed = map(ySpeed, 0, 1, -this.maxSpeed, this.maxSpeed);
+    this.yTime += this.timeShift;
+
+    this.y += ySpeed;
+    if(this.y < 0) {
+      this.y = height;
+    }
+    else if(this.y > height) {
+      this.y = 0;
+    }
+  }
+}
+
+
+
--- Assignment 2/sketch.js
+++ Assignment 2/sketch.js
@@ -3,7 +3,7 @@
 // Date
 
 
-
+//Setting the global variables 
 let noiseIncrement = 1;
 let time = 0;
 let averageHeight = 0;
@@ -14,18 +14,27 @@ function setup() {
   rectMode(CORNERS);
 }
 
+
+//Calling the functions every frame
 function draw() {
   background(255); noStroke(); fill(0);
+
   generateTerrain();
+  displayAverageHeight(averageHeight);
+  drawFlag();
 }
+
 
 function generateTerrain(){
   let listOfHeights = [];
   let biggestHeight = windowHeight;
-  noStroke();   fill(0);
+
+  noStroke(); fill(0);
+
   for (let x = 0; x < windowWidth; x += noiseIncrement){
     let noiseVariable = noise((x + time)/100);
     let mapNoisedVariable = map(noiseVariable, 0, 1, windowHeight/2 , windowHeight);
+
     listOfHeights.push([mapNoisedVariable, x]);
     rect(x, windowHeight, x + noiseIncrement, mapNoisedVariable);
   }
@@ -40,33 +49,35 @@ function generateTerrain(){
 
   averageHeight = averageHeight/listOfHeights.length;
 
-  displayAverageHeight(averageHeight);
-  drawFlag();
   time += 1;
 }
 
 function drawFlag() {
   let xCord = biggestHeightCoords[0];
   let yCord = biggestHeightCoords[1];
+
   strokeWeight(5);
   line(xCord, yCord, xCord, yCord - 50);
+
   fill(255, 0, 0);
   triangle(xCord, yCord - 25, xCord, yCord - 50, xCord + 25, yCord - 37.5);
 }
 
 function displayAverageHeight(averageHeight){
-  stroke(255,0,0);
-  strokeWeight(2);
+  stroke(255,0,0); strokeWeight(2);
+
   line(0, averageHeight, windowWidth, averageHeight);
   fill(255);
   text("Average Height = " + Math.round(Math.abs(averageHeight - windowHeight)) + " pixels", windowWidth*1/70, windowHeight*39/40);
 }
 
+
+// When the left or right arrow keys are pressed the size of the terrain rectangle changes 
 function keyPressed(){
-  if (keyCode === 37){
-    noiseIncrement -= 1;
+  if (keyCode === 37){ // Left arrow key = bigger rectangles
+    noiseIncrement *= 1.1;
   }
   else if (keyCode === 39){
-    noiseIncrement += 1;
+    noiseIncrement *= 0.9; // smaller arrow key = bigger rectangles
   }
 }
