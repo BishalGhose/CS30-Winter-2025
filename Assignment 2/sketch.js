5afa281c743df5022b3b933c8593c2500b009269
BishalGhose <bishalghose00@gmail.com>
2025-04-07 12:18:02 -0600

    Did lesson 18

--- Assignment 2/sketch.js
+++ Assignment 2/sketch.js
@@ -9,10 +9,10 @@ let time = 0;
 let averageHeight = 0;
 let biggestHeightCoords;
 
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CORNERS);
-  frameRate(10);
 }
 
 
@@ -27,19 +27,24 @@ function draw() {
 
 
 function generateTerrain(){
+
   let listOfHeights = [];
   let biggestHeight = windowHeight;
 
   noStroke(); fill(0);
 
+  // Sets a noise variable depending on the time and x-value, maps it and draws a rectangle with that variable
   for (let x = 0; x < windowWidth; x += noiseIncrement){
-    let noiseVariable = noise((x + time)/100);
+    let noiseVariable = noise((x + time)/(100 ));
     let mapNoisedVariable = map(noiseVariable, 0, 1, windowHeight/2 , windowHeight);
 
     listOfHeights.push([mapNoisedVariable, x]);
+    console.log(x, mapNoisedVariable)
     rect(x, windowHeight, x + noiseIncrement, mapNoisedVariable);
   }
 
+  /* Calculates the biggest height by looping through and comparing every height
+  and sets up the sum of all the heights as average height to calculate later*/
   for (let i of listOfHeights){
     averageHeight += i[0];
     if (i[0] < biggestHeight) {
@@ -48,11 +53,13 @@ function generateTerrain(){
     }
   }
 
-  averageHeight = averageHeight/listOfHeights.length;
+  // Calculate average height
+  averageHeight /= listOfHeights.length;
 
   time += 1;
 }
 
+// Gets the x,y of the biggest height and draws a flag based on thsoe coords
 function drawFlag() {
   let xCord = biggestHeightCoords[0] + noiseIncrement/2;
   let yCord = biggestHeightCoords[1];
@@ -64,6 +71,7 @@ function drawFlag() {
   triangle(xCord, yCord - 25, xCord, yCord - 50, xCord + 25, yCord - 37.5);
 }
 
+// Displays the average average height when given the average height
 function displayAverageHeight(averageHeight){
   stroke(255,0,0); strokeWeight(2);
 
@@ -76,9 +84,9 @@ function displayAverageHeight(averageHeight){
 // When the left or right arrow keys are pressed the size of the terrain rectangle changes 
 function keyPressed(){
   if (keyCode === 37){ // Left arrow key = bigger rectangles
-    noiseIncrement *= 1.1;
+    noiseIncrement += 1;
   }
   else if (keyCode === 39){
-    noiseIncrement *= 0.9; // smaller arrow key = bigger rectangles
+    noiseIncrement -= 1; // Right arrow = smaller rectangles
   }
 }
