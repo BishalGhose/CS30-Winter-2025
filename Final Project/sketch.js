bf06fb808cc3c98a09b68b9484d3c9c218930b17
BishalGhose <bishalghose00@gmail.com>
2025-04-10 12:18:05 -0600

    handed in assignments

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -5,23 +5,21 @@
 
 
 
+let modelObj;
 
+function preload() {
+  modelObj = loadModel("assets/untitled.obj");
+}
 
 
 function setup() {
-  createCanvas(1000, 650);
+  createCanvas(1000, 650, WEBGL);
 }
 
 function draw() {
-  background(0,255,255);
-  stroke(10,10,255);
-  strokeWeight(50);
-  line(width/2, 0, width/2 + 150, height);
-  stroke(255, 0, 255);
-  strokeWeight(5);
-  line(width/2 - 25, 0, width/2 + 125, height);
-  line(width/2 + 25, 0, width/2 + 175, height);
-  fill(0,149,255);
-  noStroke();
-  triangle(width/2 + 30, 0, width, 0, width, height);
+  background(200);
+  scale(100);
+
+  rotate(0.01 * frameCount, [0,0,0]);
+  model(modelObj);
 }
